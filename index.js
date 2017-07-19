4c734805c1a2ddb4787975b5dafe66e9c7966b3b
CreativeFingers <30295395+CreativeFingers@users.noreply.github.com>
2017-07-19 21:46:36 +0530

    1st check in
    1st check in to master

---
+++ index.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+	$("#btn1").click(function(){
+		$("#p1").show(500);
+		$("#js_img").show(500);
+		
+	})
+	$("#btn2").click(function(){
+		$("#p1").hide(500);
+		$("#js_img").hide(500);
+	});
+	
+});
